1cb557f23338ac79e5bcec7ad08cd824e600db05
Chester H <chesterh@chesters-mbp.lan>
2020-06-21 15:46:22 -0700

    Set up Skeleton App with Nuxt and Axios

---
+++ components/kasaHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>Kasa</h1>
+        <ul>
+            <li>
+                <nuxt-link to='/'>Your reservations</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to='/'>FAQ</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to='/'>Contact</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to='/'>Guest Portal</nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div v-for="re in reservations" v-bind:key="re.confirmationCode">
+      ohyea
+      {{re.checkInDate}} -- {{re.checkOutDate}}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      reservations: []
+    }
+  },
+  async created() {
+    const config = {
+      headers: {'Accept': 'application/json'}
+    }
+    try {
+      const res = await axios.get('https://my-json-server.typicode.com/ChesterHub/KasaLiving_JsonServer/reservations', config)
+      this.reservations = res.data
+    } catch (err) {
+      console.log(err)
+      }
+  }
+  
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
